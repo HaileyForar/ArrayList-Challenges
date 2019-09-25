6e98b6d41397c3a1056fdb09525989d69d5d9fc3
hailey.forar <hailey.forar@MU-C1-20.mullen.pvt.k12.co.us>
2019-09-25 11:50:52 -0600

    First commit - still working on challenges

---
+++ Array List Challenges/src/ArrayListChallenges.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListChallenges
+	{
+
+		public static void main(String[] args)
+			{
+				challenges();
+			}
+		
+		public static void challenges()
+		{
+			ArrayList <String> iceCream = new ArrayList <String>();
+			
+			iceCream.add("Rocky Road");
+			iceCream.add("Butter Pecan");
+			iceCream.add("Cotton Candy");
+			iceCream.add("Blackberry");
+			iceCream.add("Caramel Swirl");
+			iceCream.add("Coconut");
+			iceCream.add("Dark Chocolate Chunk");
+			iceCream.add("Blueberry");
+			iceCream.add("Watermelon");
+			iceCream.add("Bubblegum");
+			
+			//First Challenge
+			System.out.println("Printed Array List:");
+			System.out.println();
+			for(String s : iceCream)
+			{
+				System.out.println(s);
+			}
+			
+			//Second Challenge
+			System.out.println();
+			System.out.println("Alphabetized List:");
+			System.out.println();
+			Collections.sort(iceCream);
+			for(String s : iceCream)
+				{
+					System.out.println(s);
+				}
+			
+			//Third Challenge
+			System.out.println();
+			System.out.println("First and Last Items Alphabetically:");
+			System.out.println();
+			Collections.sort(iceCream);
+			System.out.println(iceCream.get(0) + " " + iceCream.get(9));
+				
+			
+			//Fourth Challenge
+			for(int i = 0; i < iceCream; i++)
+				{
+					
+				}
+		
+			
+			//Fifth Challenge
+		}
+		
+
+	}
